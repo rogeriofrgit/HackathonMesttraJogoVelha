437699a6f84c5e6b45ec4ec1738edb1cadefaf9e
rogeriofrgit <42914626+rogeriofrgit@users.noreply.github.com>
2024-12-11 20:02:31 -0300

    Upload inicial do codigo
    Codigo produzido na hackacton

---
+++ teste/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+            String nome;
+            Scanner teclado = new Scanner(System.in);
+
+            CharSequence vogais = "eri";
+
+            System.out.println("Rogerio de Freitas Ribeiro".contains(vogais));   
+    }
+
+
+
+}
